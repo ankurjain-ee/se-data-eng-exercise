14ef8b1d17fadae0c437e5210ff165e67768b679
Ankur Jain <ankur@equalexperts.com>
2024-11-07 11:59:41 +0530

    [Ankur] add terraform-remote-backend state

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,3 +7,10 @@ resource "google_storage_bucket" "static" {
   public_access_prevention    = "enforced"
   uniform_bucket_level_access = true
 }
+
+terraform {
+  backend "gcs" {
+    bucket  = "f7f3190898ce24b8-terraform-remote-backend"
+    prefix  = "terraform/state" // Please keep a prefix unique to your env/project
+  }
+}
